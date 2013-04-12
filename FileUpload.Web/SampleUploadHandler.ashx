a1529f3e09bd280d960f0e40d16e409a7cb1323f
Jacob Harasimo <jacobharasimo@gmail.com>
2013-04-11 21:43:30 -0400

    simple file uploaded

---
+++ FileUpload.Web/SampleUploadHandler.ashx
@@ -0,0 +1,19 @@
+﻿<%@ WebHandler Language="C#" Class="SampleUploadHandler" %>
+
+using System;
+using System.Web;
+using System.IO; 
+using Telerik.Windows;
+
+public class SampleUploadHandler : RadUploadHandler
+{
+    public override void ProcessStream()
+    {
+        base.ProcessStream();
+        if (this.IsFinalFileRequest())
+        {
+            // your custom logic for insert in DB/rename/etc.. goes here  
+        }
+    }  
+
+}
---
+++ FileUpload/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace FileUpload
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RadUploadDropPanel1_DragEnter(object sender, DragEventArgs e)
+        {
+            Color backgroundColor = new Color();
+            backgroundColor.R = 208;
+            backgroundColor.G = 232;
+            backgroundColor.B = 254;
+            this.RadUploadDropPanel1.Background = new SolidColorBrush(backgroundColor);
+        }
+        private void RadUploadDropPanel_DragLeave(object sender, DragEventArgs e)
+        {
+            this.RadUploadDropPanel1.Background = new SolidColorBrush(Colors.White);
+        }
+       
+        
+       
+
+    }
+}
